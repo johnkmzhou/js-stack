cfd89a2af3766c2cb2042347ab19e6554ebc8d97
John Zhou <john.km.zhou@gmail.com>
2017-12-03 19:36:19 -1000

    Add Bootstrap and JSS.

--- src/shared/component/Button.jsx
+++ src/shared/component/Button.jsx
@@ -8,6 +8,6 @@ type Props = {
 };
 
 const Button = ({ label, handleClick }: Props) =>
-  <button onClick={handleClick}>{label}</button>;
+  <button onClick={handleClick} className="btn btn-primary" type="button">{label}</button>;
 
 export default Button;
--- src/shared/component/page/NotFoundPage.jsx
+++ src/shared/component/page/NotFoundPage.jsx
@@ -2,11 +2,14 @@
 
 import React from 'react';
 import Helmet from 'react-helmet';
+import { Link } from 'react-router-dom';
+import { HOME_PAGE_ROUTE } from '../../routes';
 
 const title = 'Page Not Found';
 
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const NotFoundPage = () => (
-  <div>
+  <div className="container mt-4">
     <Helmet
       title={title}
       met={[
@@ -14,7 +17,13 @@ const NotFoundPage = () => (
         { property: 'og:title', content: title },
       ]}
     />
-    <h1>{title}</h1>
+    <div className="row">
+      <div className="col-12">
+        <h1>{title}</h1>
+        <div><Link to={HOME_PAGE_ROUTE}>Go to the homepage</Link>.</div>
+      </div>
+    </div>
   </div>);
+/* eslint-enable jsx-a11y/anchor-is-valid */
 
 export default NotFoundPage;
